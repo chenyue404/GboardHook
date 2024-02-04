74f110a63d07e75a59654f762aa4f5af1ac5fb69
chenyue404 <601450225@qq.com>
2024-02-04 14:15:54 +0800

    修改hook点

--- app/src/main/java/com/chenyue404/gboardhook/PluginEntry.kt
+++ app/src/main/java/com/chenyue404/gboardhook/PluginEntry.kt
@@ -1,6 +1,8 @@
 package com.chenyue404.gboardhook
 
+import android.content.Context
 import android.content.SharedPreferences
+import android.database.Cursor
 import android.net.Uri
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
@@ -87,22 +89,112 @@ class PluginEntry : IXposedHookLoadPackage {
             )
         }
 
-        tryHook("fgv#a") {
-            findAndHookMethod("fgv", classLoader, "a",
+//        tryHook("fgv#a") {
+//            findAndHookMethod("fgv", classLoader, "a",
+//                Uri::class.java,
+//                String::class.java,
+//                Array<String>::class.java,
+//                String::class.java, object : XC_MethodHook() {
+//                    override fun beforeHookedMethod(param: MethodHookParam) {
+//                        val uri = param.args.first() as Uri
+//                        val arg1 = param.args[1] as String
+//                        val arg2 = param.args[2] as Array<String>
+//                        val arg3 = param.args[3] as String
+//                        log("uri=$uri\narg1=$arg1\narg2=${arg2.joinToString()}\narg3=$arg3")
+//                        val indexOf = arg1.indexOf("timestamp >= ?")
+//                        if (indexOf != -1) {
+//                            var indexOfWen = 0
+//                            StringBuilder(arg1).forEachIndexed { index, c ->
+//                                if (index >= indexOf) return@forEachIndexed
+//                                if (c == '?') {
+//                                    indexOfWen++
+//                                }
+//                            }
+//
+//                            val afterTimeStamp = System.currentTimeMillis() - clipboardTextTime
+//                            arg2[indexOfWen] = afterTimeStamp.toString()
+//                            param.args[2] = arg2
+//                            log(
+//                                "修改时间限制, ${
+//                                    SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.ROOT)
+//                                        .format(Date(afterTimeStamp))
+//                                }"
+//                            )
+//                        }
+//                        if (arg3 == "timestamp DESC limit 5") {
+//                            param.args[3] = "timestamp DESC limit $clipboardTextSize"
+//                            log("修改大小限制, $clipboardTextSize")
+//                        }
+//                    }
+//                })
+//        }
+
+        tryHook("fhv#d(context,Collection)") {
+            findAndHookMethod(
+                "fhv", classLoader,
+                "d",
+                Context::class.java,
+                Collection::class.java,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        log("fhv#e(context,Collection)")
+                        val collection = param.args[1] as Collection<*>
+                        log(collection.joinToString())
+                    }
+                }
+            )
+        }
+
+        tryHook("com.google.android.apps.inputmethod.libs.clipboard.ClipboardContentProvider.delete(Uri,String,String[])") {
+            findAndHookMethod(
+                "com.google.android.apps.inputmethod.libs.clipboard.ClipboardContentProvider",
+                classLoader,
+                "delete",
                 Uri::class.java,
                 String::class.java,
                 Array<String>::class.java,
-                String::class.java, object : XC_MethodHook() {
+                object : XC_MethodHook() {
                     override fun beforeHookedMethod(param: MethodHookParam) {
-                        val uri = param.args.first() as Uri
-                        val arg1 = param.args[1] as String
-                        val arg2 = param.args[2] as Array<String>
-                        val arg3 = param.args[3] as String
-                        log("uri=$uri\narg1=$arg1\narg2=${arg2.joinToString()}\narg3=$arg3")
-                        val indexOf = arg1.indexOf("timestamp >= ?")
+                        log("com.google.android.apps.inputmethod.libs.clipboard.ClipboardContentProvider.delete(Uri,String,String[])")
+                        val uri = param.args[0] as Uri
+                        val str = param.args[1]
+                        val strArr = if (param.args[2] != null) {
+                            param.args[2] as Array<*>
+                        } else null
+                        log("uri=$uri, str=$str, strArr=${strArr?.joinToString() ?: "null"}")
+                    }
+                }
+            )
+        }
+
+        tryHook("com.google.android.apps.inputmethod.libs.clipboard.ClipboardContentProvider.query") {
+            findAndHookMethod(
+                "com.google.android.apps.inputmethod.libs.clipboard.ClipboardContentProvider",
+                classLoader,
+                "query",
+                Uri::class.java,
+                Array<String>::class.java,
+                String::class.java,
+                Array<String>::class.java,
+                String::class.java,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        log("com.google.android.apps.inputmethod.libs.clipboard.ClipboardContentProvider.query")
+                        val arg0 = param.args[0] as Uri
+                        val arg1 = if (param.args[1] != null) {
+                            param.args[1] as Array<*>
+                        } else null
+                        val arg2 = param.args[2].toString()
+                        val arg3 = if (param.args[3] != null) {
+                            param.args[3] as Array<String>
+                        } else null
+                        val arg4 = param.args[4]
+                        log("query, arg0=$arg0, arg1=${arg1?.joinToString()}, arg2=$arg2, arg3=${arg3?.joinToString()}, arg4=$arg4")
+
+                        val indexOf = arg2.indexOf("timestamp >= ?")
                         if (indexOf != -1) {
                             var indexOfWen = 0
-                            StringBuilder(arg1).forEachIndexed { index, c ->
+                            StringBuilder(arg2).forEachIndexed { index, c ->
                                 if (index >= indexOf) return@forEachIndexed
                                 if (c == '?') {
                                     indexOfWen++
@@ -110,8 +202,10 @@ class PluginEntry : IXposedHookLoadPackage {
                             }
 
                             val afterTimeStamp = System.currentTimeMillis() - clipboardTextTime
-                            arg2[indexOfWen] = afterTimeStamp.toString()
-                            param.args[2] = arg2
+                            arg3?.let {
+                                it[indexOfWen] = afterTimeStamp.toString()
+                                param.args[3] = it
+                            }
                             log(
                                 "修改时间限制, ${
                                     SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.ROOT)
@@ -119,14 +213,62 @@ class PluginEntry : IXposedHookLoadPackage {
                                 }"
                             )
                         }
-                        if (arg3 == "timestamp DESC limit 5") {
-                            param.args[3] = "timestamp DESC limit $clipboardTextSize"
+                        if (arg4 == "timestamp DESC limit 5") {
+                            param.args[4] = "timestamp DESC limit $clipboardTextSize"
                             log("修改大小限制, $clipboardTextSize")
+                        }
+                    }
+
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        log("query end, ${(param.result as Cursor).count}")
+                    }
+                }
+            )
+        }
+
+        tryHook("qcy#a") {
+            findAndHookMethod("qcy", classLoader, "a",
+                String::class.java,
+                Boolean::class.java,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        return
+//                        log("qcy#a")
+                        val str = param.args[0].toString()
+                        val value = param.args[1] as Boolean
+//                        log("qcy#a, str=$str, value=$value")
+                        val modify = when (str) {
+                            "enable_clipboard_entity_extraction",
+                            "enable_settings_search",
+                            "enable_settings_two_pane_display",
+                            -> true
+
+                            else -> null
+                        }
+                        if (modify == true) {
+                            log("qcy#a, str=$str, value=$value")
+                            param.args[1] = true
+                        }
+                    }
+
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        if (param.args[0] == "enable_clipboard_entity_extraction") {
+                            log("qcy#a, ${param.result}")
                         }
                     }
                 })
         }
 
+        tryHook("fij#b") {
+            findAndHookMethod("fij", classLoader, "b",
+                Uri::class.java,
+                Int::class.java,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        log("fij#b, uri=${param.args[0]}, i=${param.args[1]}")
+                    }
+                })
+        }
     }
 
     private fun tryHook(logStr: String, unit: (() -> Unit)) {
